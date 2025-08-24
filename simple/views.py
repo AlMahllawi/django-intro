8ef099aacc91892b3df712648af1af06de4de609
Mohammad AlMahllawi <mohammadalmahllawi@gmail.com>
2025-08-24 19:34:49 +0300

    `pagetitle` variable

--- simple/views.py
+++ simple/views.py
@@ -4,11 +4,11 @@ def index(request):
     return render(request, 'simple/index.html')
 
 def clock(request):
-    return render(request, 'simple/clock.html')
+    return render(request, 'simple/clock.html', { 'pagetitle': 'Wall Clock' })
 
 def unit_convert(request):
-    return render(request, 'simple/unit-convert.html')
+    return render(request, 'simple/unit-convert.html', { 'pagetitle': 'Unit Converter' })
 
 def hangman(request):
-    return render(request, 'simple/hangman.html')
+    return render(request, 'simple/hangman.html', { 'pagetitle': 'Hangman Game' })
 
