f07ac77c16d3acde8607521d1c59cbf15a289213
Mohammad AlMahllawi <mohammadalmahllawi@gmail.com>
2025-08-26 20:30:30 +0300

    add string representation for models

--- simple/models.py
+++ simple/models.py
@@ -4,6 +4,12 @@ class HangmanWord(models.Model):
     word = models.CharField()
     category = models.CharField()
 
+    def __str__(self):
+        return self.category + ': ' + self.word
+
 class Saying(models.Model):
     content = models.TextField()
     valid = models.BooleanField()
+
+    def __str__(self):
+        return f"[{'Valid' if self.valid else 'Invalid'}] {self.content}"
