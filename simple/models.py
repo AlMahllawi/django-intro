01b525100fdcc6dbc97e3e50bcbb360b67a23eaa
Mohammad AlMahllawi <mohammadalmahllawi@gmail.com>
2025-08-26 21:27:06 +0300

    specify fields arguments

--- simple/models.py
+++ simple/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class HangmanWord(models.Model):
-    word = models.CharField()
-    category = models.CharField()
+    word = models.CharField(primary_key=True, max_length=25)
+    category = models.CharField(max_length=50)
 
     def __str__(self):
         return self.category + ': ' + self.word
@@ -11,8 +11,8 @@ class HangmanWord(models.Model):
         ordering = ['category']
 
 class Saying(models.Model):
-    content = models.TextField()
-    valid = models.BooleanField()
+    content = models.TextField(unique=True)
+    valid = models.BooleanField(default=True)
 
     def __str__(self):
         return f"[{'Valid' if self.valid else 'Invalid'}] {self.content}"
